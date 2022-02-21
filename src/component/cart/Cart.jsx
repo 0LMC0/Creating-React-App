6a7febe11da0861112ca437165246979370ebf88
Lautaro <lautycaceresss871@gmail.com>
2022-02-21 16:34:05 -0300

    Last commit

--- src/component/cart/Cart.jsx
+++ src/component/cart/Cart.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import { useCartContext } from '../../Context/cartContext';
-import { Container, Card} from 'react-bootstrap'
 import CartItem from './CartItem';
-import { Link } from 'react-router-dom';
+
 
 const Cart = () => {
-    
-    const { nCantidad } = useCartContext();
-        
- 
-        
-
-    return (
+      return (
         <div>
                 <CartItem />
         </div>
